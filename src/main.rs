e9901f2f9173fe717d5662b02602694197063032
Eyal Kalderon <ebkalderon@gmail.com>
2020-06-20 17:55:08 +0800

    Move image and viewport values to constants

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,28 @@ use vec3::{Color, Point3, Vec3};
 mod ray;
 mod vec3;
 
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
+const IMAGE_WIDTH: u32 = 384;
+const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+
+const VIEWPORT_HEIGHT: f64 = 2.0;
+const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+const FOCAL_LENGTH: f64 = 1.0;
+
 fn main() {
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 384;
-    let image_height = (image_width as f64 / aspect_ratio) as u32;
-
-    println!("P3\n{} {}\n255", image_width, image_height);
-
-    let viewport_height = 2.0;
-    let viewport_width = aspect_ratio * viewport_height;
-    let focal_length = 1.0;
+    println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     let origin = Point3::zeros();
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
     let lower_left_corner =
-        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, focal_length);
+        origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
 
-    for j in (0..image_height).rev() {
+    for j in (0..IMAGE_HEIGHT).rev() {
         eprint!("\rScanlines remaining: {:0>3}", j);
-        for i in 0..image_width {
-            let screen_x = i as f64 / (image_width - 1) as f64;
-            let screen_y = j as f64 / (image_height - 1) as f64;
+        for i in 0..IMAGE_WIDTH {
+            let screen_x = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let screen_y = j as f64 / (IMAGE_HEIGHT - 1) as f64;
             let ray = Ray::new(
                 origin,
                 lower_left_corner + (screen_x * horizontal) + (screen_y * vertical) - origin,
