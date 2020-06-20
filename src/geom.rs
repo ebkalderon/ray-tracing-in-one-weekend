85bc5864fca22df9622a3b1f754d3ccc26e1a119
Eyal Kalderon <ebkalderon@gmail.com>
2020-06-20 23:49:45 +0800

    Implement Hittable for AsRef<[Box<dyn Hittable>]>
    This commit allows us to hit a list of objects with a single ray and
    have it return the `HitRecord` of the nearest object that was hit, if
    any. This data structure will be useful to us in later commits.

--- src/geom.rs
+++ src/geom.rs
@@ -9,6 +9,21 @@ pub trait Hittable {
     fn hit(&self, ray: Ray, t_range: (f64, f64)) -> Option<HitRecord>;
 }
 
+impl<T: AsRef<[Box<dyn Hittable>]>> Hittable for T {
+    fn hit(&self, ray: Ray, (t_min, t_max): (f64, f64)) -> Option<HitRecord> {
+        let mut closest_so_far: Option<HitRecord> = None;
+
+        for object in self.as_ref().iter() {
+            let t_max = closest_so_far.as_ref().map(|rec| rec.t).unwrap_or(t_max);
+            if let Some(record) = object.hit(ray, (t_min, t_max)) {
+                closest_so_far = Some(record);
+            }
+        }
+
+        closest_so_far
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub struct HitRecord {
     pub point: Point3,
@@ -36,5 +51,65 @@ impl HitRecord {
         };
 
         HitRecord::new(point, normal, t, is_front_face)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 384;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+
+    const VIEWPORT_HEIGHT: f64 = 2.0;
+    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f64 = 1.0;
+
+    fn generate_world() -> Vec<Box<dyn Hittable>> {
+        vec![
+            Box::new(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5)),
+            Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)),
+        ]
+    }
+
+    fn compute_ray(pixel_x: u32, pixel_y: u32) -> Ray {
+        let origin = Point3::zeros();
+        let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+        let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
+        let lower_left_corner =
+            origin - (horizontal / 2.0) - (vertical / 2.0) - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+
+        let screen_x = pixel_x as f64 / (IMAGE_WIDTH - 1) as f64;
+        let screen_y = pixel_y as f64 / (IMAGE_HEIGHT - 1) as f64;
+
+        Ray::new(
+            origin,
+            lower_left_corner + (screen_x * horizontal) + (screen_y * vertical) - origin,
+        )
+    }
+
+    #[test]
+    fn selects_closest_hit() {
+        let world = generate_world();
+        let center_of_image = compute_ray(IMAGE_WIDTH / 2, IMAGE_HEIGHT / 2);
+
+        let hit_record = world
+            .hit(center_of_image, (0.0, f64::MAX))
+            .expect("ray didn't hit any objects");
+
+        assert!(hit_record.is_front_face);
+        assert!((0.00232..0.00233).contains(&hit_record.point.x));
+        assert!((0.00232..0.00233).contains(&hit_record.point.y));
+        assert!((-0.50002..-0.50001).contains(&hit_record.point.z));
+    }
+
+    #[test]
+    fn returns_none_if_hits_nothing() {
+        let world = generate_world();
+        let random_ray = Ray::new(Point3::new(0.0, 200.0, 0.0), Vec3::new(0.0, 200.0, 0.0));
+
+        let hit_record = world.hit(random_ray, (0.0, f64::MAX));
+        assert_eq!(hit_record, None);
     }
 }
