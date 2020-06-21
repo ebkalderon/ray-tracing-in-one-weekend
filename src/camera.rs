2df8bc7137231936941fef42b4658108ec062d4b
Eyal Kalderon <ebkalderon@gmail.com>
2020-06-22 00:50:01 +0800

    Implement camera defocus blur
    This commit eliminates the simple constructors from `Camera` in order to
    accommodate the new defocus blur effect (aka "depth of field" in
    photography terms).
    
    Normally, all rays projected through the scene originate from the
    `look_from` point. To accomplish defocus blur, we instead generate the
    scene rays from inside a wide disc centered at the `look_from` point,
    like a camera's aperture. The larger the radius of this "aperture" disc,
    the greater the defocus blur. You can think of our original camera as
    having a defocus disc of radius zero (no blur at all), so all rays
    originated at the disc center (`look_from`).

--- src/camera.rs
+++ src/camera.rs
@@ -7,25 +7,21 @@ pub struct Camera {
     pub lower_left_corner: Point3,
     pub horizontal: Vec3,
     pub vertical: Vec3,
+    pub u: Vec3,
+    pub v: Vec3,
+    pub w: Vec3,
+    pub lens_radius: f64,
 }
 
 impl Camera {
     pub fn new(
-        look_from: Point3,
-        look_at: Point3,
-        vertical_fov_deg: f64,
-        aspect_ratio: f64,
-    ) -> Self {
-        let up_vec = Vec3::new(0.0, 1.0, 0.0);
-        Camera::with_up_vec(up_vec, look_from, look_at, vertical_fov_deg, aspect_ratio)
-    }
-
-    pub fn with_up_vec(
         up_vec: Vec3,
         look_from: Point3,
         look_at: Point3,
         vertical_fov_deg: f64,
         aspect_ratio: f64,
+        aperture: f64,
+        focus_dist: f64,
     ) -> Self {
         let theta = vertical_fov_deg.to_radians();
         let h = (theta / 2.0).tan();
@@ -37,31 +33,45 @@ impl Camera {
         let v = w.cross(u);
 
         let origin = look_from;
-        let horizontal = viewport_width * u;
-        let vertical = viewport_height * v;
+        let horizontal = focus_dist * viewport_width * u;
+        let vertical = focus_dist * viewport_height * v;
 
         Camera {
             origin,
             horizontal,
             vertical,
-            lower_left_corner: origin - horizontal / 2.0 - vertical / 2.0 - w,
+            u,
+            v,
+            w,
+            lower_left_corner: origin - horizontal / 2.0 - vertical / 2.0 - focus_dist * w,
+            lens_radius: aperture / 2.0,
         }
     }
 
     pub fn ray_at(&self, screen_x: f64, screen_y: f64) -> Ray {
-        let (u, v) = (screen_x, screen_y);
-        let dir = self.lower_left_corner + u * self.horizontal + v * self.vertical - self.origin;
-        Ray::new(self.origin, dir)
+        let (s, t) = (screen_x, screen_y);
+        let rd = self.lens_radius * Vec3::random_in_unit_disk();
+        let offset = self.u * rd.x + self.v * rd.y;
+
+        Ray::new(
+            self.origin + offset,
+            self.lower_left_corner + s * self.horizontal + t * self.vertical - self.origin - offset,
+        )
     }
 }
 
 impl Default for Camera {
     fn default() -> Self {
+        let look_from = Point3::new(3.0, 3.0, 2.0);
+        let look_at = Point3::new(0.0, 0.0, -1.0);
         Camera::new(
-            Point3::new(-2.0, 2.0, 1.0),
-            Point3::new(0.0, 0.0, -1.0),
-            90.0,
+            Vec3::new(0.0, 1.0, 0.0),
+            look_from,
+            look_at,
+            20.0,
             16.0 / 9.0,
+            2.0,
+            (look_from - look_at).len(),
         )
     }
 }
